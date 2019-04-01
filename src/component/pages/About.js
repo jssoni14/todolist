e30120b2dd1482332288523fe16d61ae721643f8
Jay <jsoni14@gmail.com>
2019-03-31 20:14:28 -0500

    first commit

---
+++ src/component/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About(){
+  return (
+    <React.Fragment>
+
+    <h1> About </h1>
+    <p> This todolist app v1.0</p>
+
+    </React.Fragment>
+  )
+}
